5a4d59d04a1b26e5a3ea1aee692959d40c25cae0
faridhakanda <faridakanda1729@gmail.com>
2026-04-26 11:43:33 +0600

    after modified in auth

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -4,6 +4,9 @@ const Register = () => {
     return (
         <div>
             <h2>Register</h2>
+            <div>
+                <h2>Register Page Design!</h2>
+            </div>
         </div>
     );
 };
